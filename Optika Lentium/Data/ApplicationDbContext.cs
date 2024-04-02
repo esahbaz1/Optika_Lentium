b5002b33d76145f21e89d7a98c5a6572bea7a65f
nkovacevic3 <nkovacevic3@etf.unsa.ba>
2024-04-02 17:11:12 +0200

    pocetna aplikacija

---
+++ Optika Lentium/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Optika_Lentium.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
